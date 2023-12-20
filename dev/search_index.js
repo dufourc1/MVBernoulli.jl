5c3c8c04b25e1fa1008920293a3a69e3c0eb31af
Documenter.jl <documenter@juliadocs.github.io>
2023-12-20 06:49:20 +0000

    build based on e137881

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = multivarBernoulli","category":"page"},{"location":"#multivarBernoulli","page":"Home","title":"multivarBernoulli","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for multivarBernoulli.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [multivarBernoulli]","category":"page"}]
+}
