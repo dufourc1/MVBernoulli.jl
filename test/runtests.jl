5f94b5dfc43d83dee2e0fde34f47a3696528363a
dufourc1 <dufourc115@gmail.com>
2023-12-20 08:07:12 +0100

    add Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using multivarBernoulli
 using Test
+using Aqua
 
 @testset "multivarBernoulli.jl" begin
+    @testset "Code quality (Aqua.jl)" begin
+        Aqua.test_all(multivarBernoulli; deps_compat = (check_extras = false,),)
+    end
     # Write your tests here.
 end
