4f0fdf53af52deeb869b774b3f4bb94d7e3c6ee6
dufourc1 <dufourc115@gmail.com>
2024-05-09 12:02:34 +0200

    bitarrays instead of vec{Int}

--- src/utils/indices.jl
+++ src/utils/indices.jl
@@ -1,17 +1,16 @@
 function index_to_binary_vector(index::Int, size::Int)
-    result = zeros(Int, size)
+    result = falses(size)
     index_to_binary_vector!(result, index)
     return result
 end
 
-function index_to_binary_vector!(binary_vector::AbstractVector{T}, index::Int) where {T}
+function index_to_binary_vector!(binary_vector, index)
     digits!(binary_vector, index - 1; base = 2)
-    reverse!(binary_vector)
 end
 
-function binary_vector_to_index(binary_vector::AbstractVector{T}) where {T}
+function binary_vector_to_index(binary_vector)
     index = 1
-    @inbounds for (i, s) in enumerate(Iterators.reverse(binary_vector))
+    @inbounds for (i, s) in enumerate(binary_vector)
         index += s * 2^(i - 1)
     end
     return index
